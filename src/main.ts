80d8098306deda250193f8c6a69c0d5d9ca4b779
SoftLaptop <MohamedSalah50>
2026-01-19 20:33:14 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { ValidationPipe } from '@nestjs/common';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+      origin: 'http://localhost:5173',
+      credentials: true,
+    });
 
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: true, limit: '50mb' }));
